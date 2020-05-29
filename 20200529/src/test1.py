0c95787e5ba6bdd38dcaa65c29b0914fab39ce2a
Melody-Gai <539263156@qq.com>
2020-05-29 22:51:38 +0800

    python简单运行脚本

---
+++ 20200529/src/test1.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Firefox()
+driver.get("https://www.baidu.com/")
+driver.find_element_by_id("kw").send_keys("乃万")
+driver.find_element_by_id("su").submit()
+time.sleep(6)
+driver.find_element_by_link_text("乃万_百度百科").click()
+time.sleep(5)
+driver.quit()
